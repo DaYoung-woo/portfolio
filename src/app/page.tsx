4cfca6a420fafdcfb4a19c5917fa2573b6e207cb
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-10-17 22:31:44 +0900

    [refactor] contact 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { FaGithub, FaExternalLinkAlt, FaGraduationCap } from "react-icons/fa";
-import { SiVelog } from "react-icons/si";
 import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,6 +11,7 @@ import About from "@/components/About";
 import Thumb from "@/components/Thumb";
 import Career from "@/components/Career";
 import Certification from "@/components/Certification";
+import Contact from "@/components/Contact";
 const MotionImage = motion(Image);
 
 interface ActivityImageProps {
@@ -459,54 +459,7 @@ export default function Home() {
         </motion.section>
 
         <Certification />
-
-        {/* Contact Section */}
-        <motion.section
-          id="contact"
-          className="py-20"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
-        >
-          <div className="container mx-auto px-6 text-center">
-            <h2 className="text-4xl font-bold mb-8 text-emerald-500 mb-8">
-              Get In Touch
-            </h2>
-
-            <div className="flex justify-center space-x-4">
-              <motion.a
-                href="https://github.com/DaYoung-Woo"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-3xl text-emerald-500 hover:text-emerald-600"
-                whileHover={{ scale: 1.2, rotate: 360 }}
-                transition={{ type: "spring", stiffness: 260, damping: 20 }}
-              >
-                <FaGithub />
-              </motion.a>
-              <motion.a
-                href="https://velog.io/@iamwooda"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-3xl text-emerald-500 hover:text-emerald-600"
-                whileHover={{ scale: 1.2, rotate: 360 }}
-                transition={{ type: "spring", stiffness: 260, damping: 20 }}
-              >
-                <SiVelog />
-              </motion.a>
-            </div>
-            <motion.p
-              className="text-xl text-gray-700 dark:text-gray-300 mt-12 mb-8"
-              initial={{ y: 20, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              transition={{ delay: 0.2, duration: 0.5 }}
-              viewport={{ once: true }}
-            >
-              읽어주셔서 감사합니다.
-            </motion.p>
-          </div>
-        </motion.section>
+        <Contact />
       </main>
     </div>
   );
--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
 import React from "react";
+import { motion } from "framer-motion";
 import { FaBuilding } from "react-icons/fa";
 
 const techList = [

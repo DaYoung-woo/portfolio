f15b34377d6a114d835626661669eb5fc5a2c010
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-10-17 23:31:10 +0900

    [refactor]activity 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,10 @@ import Nav from "@/components/Nav";
 import About from "@/components/About";
 import Thumb from "@/components/Thumb";
 import Career from "@/components/Career";
+import Activity from "@/components/Activity";
 import Certification from "@/components/Certification";
 import Contact from "@/components/Contact";
 const MotionImage = motion(Image);
-
-interface ActivityImageProps {
-  src: string;
-  alt: string;
-}
-
-const ActivityImage: React.FC<ActivityImageProps> = ({ src, alt }) => (
-  <div className="relative w-full max-w-[680px] mx-auto aspect-video">
-    <MotionImage
-      src={src}
-      alt={alt}
-      fill
-      sizes="(max-width: 680px) 100vw, 680px"
-      style={{ objectFit: "contain" }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    />
-  </div>
-);
 
 const project = {
   title: "🐽 PIGGY",
@@ -82,22 +63,6 @@ const project = {
     "상점 물품 리스트",
   ],
 };
-
-const activities = [
-  {
-    title: "클라우딩 어플리케이션 엔지니어링 데브코스",
-    description: `클라우드 애플리케이션 엔지니어링 데브코스 수업을 통해 React와 React Native에 대한 숙련도를 높였습니다.\n 
-    열정적으로 참여하여 개인 프로젝트에서 좋은 결과를 도출하여 발표를 진행했습니다.`,
-    videoLink: "https://www.youtube.com/embed/0f9AZ6O93Ig", // Replace with actual link
-    experience: "2024.04 - 2024.09",
-  },
-  {
-    title: "2024 오픈소스 컨트리뷰션 아카데미",
-    description:
-      "git 연동이 안 된 프로젝트에서 무한로딩이 발생하는 증상을 발견하여 이슈를 리포트했습니다.\n 같이 활동을 하는 팀원과 함께 문제를 해결하여 풀리퀘스트를 생성했습니다.",
-    link: "https://github.com/your-contribution-link", // Replace with actual link
-  },
-];
 
 interface CustomSliderSettings
   extends Omit<Settings, "appendDots" | "customPaging"> {
@@ -277,187 +242,7 @@ export default function Home() {
           </div>
         </motion.section>
 
-        {/* Activities Section */}
-        <motion.section
-          id="activities"
-          className="py-20"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
-        >
-          <div className="container mx-auto px-6">
-            <h2 className="text-4xl font-bold mb-8 text-emerald-500">
-              Activities
-            </h2>
-            {activities.map((activity, index) => (
-              <motion.div
-                key={index}
-                className="mb-12 bg-gray-50 dark:bg-gray-700 rounded-lg shadow-lg p-6"
-                initial={{ y: 50, opacity: 0 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                transition={{ delay: index * 0.1, duration: 0.5 }}
-                viewport={{ once: true }}
-              >
-                <div className="flex items-center mb-4">
-                  <FaGraduationCap className="text-2xl text-emerald-500 mr-2" />
-                  <h3 className="text-2xl font-semibold">{activity.title}</h3>
-                </div>
-
-                {activity.videoLink && (
-                  <>
-                    <h5 className="text-gray-600 dark:text-gray-400 font-semibold">
-                      {activity.experience}
-                    </h5>
-                    <p className="mb-4">
-                      클라우드 애플리케이션 엔지니어링 데브코스 수업을 통해
-                      React와 React Native에 대한 숙련도를 높였습니다.
-                    </p>
-
-                    <div className="mt-8">
-                      <div>
-                        <h4 className="text-xl font-bold">🎬 비디오 에디터</h4>
-                        <p className="text-gray-700 dark:text-gray-300">
-                          비디오 에디터는 영상을 gif, mp4, mp3 파일료 변환해주는
-                          웹어플리케이션입니다.
-                        </p>
-
-                        <div className="flex mt-4">
-                          <a
-                            className="flex items-center text-emerald-500 hover:underline "
-                            href="https://video-editor-dayoung.netlify.app"
-                          >
-                            <FaExternalLinkAlt className="mr-2" /> Live Demo
-                          </a>
-                          <a
-                            className="flex items-center text-emerald-500 hover:underline ml-2"
-                            href="https://video-editor-dayoung.netlify.app"
-                          >
-                            <FaGithub className="mr-2" /> View on GitHub
-                          </a>
-                        </div>
-
-                        {/* <h4 className="text-lg font-semibold mt-4 mb-2">
-                          사용 기술
-                        </h4>
-                        <div className="mb-4">
-                          {[
-                            "react",
-                            "Vite",
-                            "Chakra",
-                            "TailwindCSS",
-                            "FFmpeg",
-                          ].map((tech, index) => (
-                            <motion.span
-                              key={tech}
-                              className="bg-emerald-50 dark:bg-emerald-900 text-emerald-800 dark:text-emerald-100 px-3 py-1 rounded-full text-sm mr-2 mb-2"
-                              initial={{ opacity: 0, x: -20 }}
-                              animate={{ opacity: 1, x: 0 }}
-                              transition={{ delay: index * 0.1 }}
-                            >
-                              {tech}
-                            </motion.span>
-                          ))}
-                        </div> */}
-
-                        {/* <div className="relative w-full">
-                          <div className="max-w-[768px] max-h-[365px] mx-auto">
-                            <Slider {...sliderSettings}>
-                              {[
-                                "/videoEditor1.png",
-                                "/videoEditor2.png",
-                                "/videoEditor3.png",
-                                "/videoEditor4.png",
-                                "/videoEditor5.png",
-                              ].map((image, index) => (
-                                <div key={index} className="w-full h-full">
-                                  <img
-                                    src={image}
-                                    alt={`Video Editor ${index + 1}`}
-                                    className="w-full h-full object-cover"
-                                  />
-                                </div>
-                              ))}
-                            </Slider>
-                          </div>
-                        </div> */}
-
-                        <p className="text-gray-700 dark:text-gray-300 mb-4 mt-8">
-                          비디오 에디터 프로젝트는 좋은 평가를 받아 발표를
-                          진행하였습니다.
-                        </p>
-                        <div className="max-w-[768px] mx-auto">
-                          <div
-                            className="relative w-full"
-                            style={{ aspectRatio: "16 / 9" }}
-                          >
-                            <motion.iframe
-                              src={activity.videoLink}
-                              frameBorder="0"
-                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                              allowFullScreen
-                              className="absolute top-0 left-0 w-full h-full"
-                              initial={{ opacity: 0, scale: 0.8 }}
-                              whileInView={{ opacity: 1, scale: 1 }}
-                              transition={{ duration: 0.5 }}
-                              viewport={{ once: true }}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                )}
-                {activity.link && (
-                  <>
-                    <p className="text-gray-700 dark:text-gray-300">
-                      vscode의 익스텐션 중 하나로 Git 히스토리와 커밋, 기여
-                      내역을 시각적으로 쉽게 탐색할 수 있게 해주는 githru
-                      오픈소스에 참여하였습니다.
-                    </p>
-                    <p className="text-gray-700 dark:text-gray-300">
-                      git을 기반으로 동작하는 익스텐션이므로 git이 연동되지 않은
-                      프로젝트에서 githru를 사용하는 경우 무한 로딩이 발생하는
-                      것을 확인하였습니다.
-                    </p>
-                    <p className="text-gray-700 dark:text-gray-300">
-                      이 증상은 사용자 경험에 부정적인 영향을 미칠 수 있어 해당
-                      이슈를 리포트했습니다.
-                    </p>
-                    <a
-                      className="flex items-center text-emerald-500 hover:underline mt-2 mb-4"
-                      href="https://github.com/githru/githru-vscode-ext/issues/520"
-                    >
-                      <FaExternalLinkAlt className="mr-2" /> View Issue
-                    </a>
-                    <ActivityImage src="/issue.png" alt="issue" />
-
-                    <p className="text-gray-700 dark:text-gray-300 mt-12">
-                      해당 이슈를 같이 활동을 하는 팀원과 함께 짝코딩을 통해
-                      문제를 해결하였습니다.
-                    </p>
-                    <p className="text-gray-700 dark:text-gray-300">
-                      무한 로딩을 막고 하단 오른쪽에 Git이 연결되어 있지 않다는
-                      메시지를 보여주도록 변경하였습니다.
-                    </p>
-                    <p className="text-gray-700 dark:text-gray-300">
-                      또한 하단 왼쪽에 로딩 아이콘을 X아이콘으로 변경되도록
-                      설정하여 풀리퀘스트를 생성했습니다.
-                    </p>
-                    <a
-                      className="flex items-center text-emerald-500 hover:underline mt-2 mb-4"
-                      href="https://github.com/githru/githru-vscode-ext/pull/679"
-                    >
-                      <FaExternalLinkAlt className="mr-2" /> View Pull Request
-                    </a>
-                    <ActivityImage src="/fix.png" alt="fix" />
-                  </>
-                )}
-              </motion.div>
-            ))}
-          </div>
-        </motion.section>
-
+        <Activity />
         <Certification />
         <Contact />
       </main>
